60fc032cbb9266a5763ce612b5c395101bbaa4f4
Pasindu Madhuranga <45681668+Pasindurathnasiri@users.noreply.github.com>
2020-10-31 08:38:03 +0530

    Initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AddUserComponent} from './add-user/add-user.component';
+import {ViewUserComponent} from './view-user/view-user.component';
+import {HomeComponent} from './home/home.component';
+const routes: Routes = [
+{path:'add-user',component:AddUserComponent},
+{path:'view-user',component:ViewUserComponent},
+{path:'',component:HomeComponent}
 
-const routes: Routes = [];
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
